22e9858658fe1a57ee1cdc04337371503ab46f9c
Davit <davidseraydaryan@gmail.com>
2025-06-19 21:00:54 +0200

    new pokemon types!!

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 from random import randint
 import requests
+import random
 
 class Pokemon:
     pokemons = {}
@@ -7,16 +8,25 @@ class Pokemon:
     def __init__(self, pokemon_trainer):
 
         self.pokemon_trainer = pokemon_trainer   
-
+       
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.ability = self.get_ability()
+        self.hp = random.randint(200, 400)
+        self.power = random.randint(30, 60)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['front_default'])
+        else:
+            return "error"
     
     # Метод для получения имени покемона через API
     def get_name(self):
@@ -28,14 +38,64 @@ class Pokemon:
         else:
             return "Pikachu"
 
-
+    def get_ability(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['abilities'][0]['ability']['name'])
+        else:
+            return "error"
+        
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        pokemon_info = (
+
+            f"Имя твоего покеомона: {self.name}/n"
+            f"pokemon power: {self.power}/n"
+            f"pokemon: {self.hp}"
+        )
+        return pokemon_info
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+    def ab(self):
+        return self.ability
+    
+    # class's method for attack
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = randint(1, 5)
+            if chance  == 2:
+                return f'pokemon-wizard used magic shield'
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f'fight between @{self.pokemon_trainer} and @{enemy.pokemon_trainer}'
+        else:
+            enemy.hp = 0
+            return f'@{self.pokemon_trainer} won against @{enemy.pokemon_trainer}'
 
+class Wizard(Pokemon):
+    pass
+
+class Fighter(Pokemon): # 30-60 / 6-12
+    def attack(self, enemy):
+        super_power = randint(5, 15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f'/nfighter used its power {super_power}'
+
+if __name__ == '__main__':
+    wizard = Wizard("username1")
+    fighter = Fighter("username2")
+
+    print(wizard.info())
+    print()
+    print(fighter.info())
+    print()
+    print(fighter.attack(wizard))
 
 
--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import telebot 
 from config import token
 
-from logic import Pokemon
+from logic import Pokemon, Wizard, Fighter
 
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['go'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+        chance = (1, 5)
+        if chance == 1 or 2 or 3:
+            pokemon = Pokemon(message.from_user.username)
+        elif chance == 4:
+            pokemon = Wizard(message.from_user.username)
+        elif chance ==5:
+            pokemon = Fighter(message.from_user.usernamer)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
+        bot.send_message(message.chat.id, pokemon.ab())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
+
+@bot.message_handler(commands=['attack'])
+def attacks(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            yourpok = Pokemon.pokemons[message.from_user.username]
+            res = yourpok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+
+        else:
+            bot.send_message(message.chat.id, 'You can only fight with pokemons')
+    else:
+        bot.send_message(message.chat.id, "You have to reply to a certain person's message to attack")
 
 
 bot.infinity_polling(none_stop=True)
